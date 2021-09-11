3abde9ebeb7f534cdc4fbeb1b1cffdfa215e03ec
marunemo <chys3697@gmail.com>
2021-09-11 15:55:20 +0900

    Implement Student and Fruit Comparator

--- src/prpj_week2/fruit/FruitApp.java
+++ src/prpj_week2/fruit/FruitApp.java
@@ -1,5 +1,59 @@
 package prpj_week2.fruit;
 
-public class FruitApp {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+public class FruitApp {
+	int no;			// number
+	String name;	// name
+	int price;		// price
+	
+	public FruitApp(int no, String name, int price) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.price = price;
+	}
+
+	public static class AscComparator implements Comparator<FruitApp> {
+		@Override
+		public int compare(FruitApp o1, FruitApp o2) {
+			return o1.name.compareTo(o2.name);
+		}
+	}
+	
+	public static class DescComparator implements Comparator<FruitApp> {
+		@Override
+		public int compare(FruitApp o1, FruitApp o2) {
+			return o2.name.compareTo(o1.name);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruit [no=" + this.no + ", name=" + this.name +  ", price=" + this.price + "]";
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<FruitApp> fruit = new ArrayList<FruitApp>();
+		fruit.add(new FruitApp(1, "apple", 3500));
+		fruit.add(new FruitApp(2, "strawberry", 2000));
+		fruit.add(new FruitApp(3, "grapes", 5000));
+		fruit.add(new FruitApp(4, "orange", 3000));
+		fruit.add(new FruitApp(5, "lemon", 4500));
+		fruit.add(new FruitApp(6, "kiwi", 3500));
+		fruit.add(new FruitApp(7, "cherry", 3000));
+		fruit.add(new FruitApp(8, "cranberry", 3500));
+		fruit.add(new FruitApp(9, "guava", 5000));
+		fruit.add(new FruitApp(10, "mango", 2500));
+		
+		System.out.println("Friut List (ordered by name)");
+		Collections.sort(fruit, new FruitApp.AscComparator());
+		fruit.forEach(System.out::println);
+		
+		System.out.println("Friut List (reverse ordered by name)");
+		Collections.sort(fruit, new FruitApp.DescComparator());
+		fruit.forEach(System.out::println);	
+	}
 }
--- src/prpj_week2/student/StudentApp.java
+++ src/prpj_week2/student/StudentApp.java
@@ -1,5 +1,50 @@
 package prpj_week2.student;
 
-public class StudentApp {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StudentApp implements Comparable<StudentApp> {
+	int no;			// number
+	String name;	// name
+	int age;		// age
+	
+	public StudentApp(int no, String name, int age) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public int compareTo(StudentApp o) {
+		return name.compareTo(o.name);
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [no=" + this.no + ", name=" + this.name +  ", age=" + this.age + "]";
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<StudentApp> student = new ArrayList<StudentApp>();
+		student.add(new StudentApp(1, "Kim", 3500));
+		student.add(new StudentApp(2, "Lee", 2000));
+		student.add(new StudentApp(3, "Park", 5000));
+		student.add(new StudentApp(4, "Choe", 3000));
+		student.add(new StudentApp(5, "Seong", 4500));
+		student.add(new StudentApp(6, "Han", 3500));
+		student.add(new StudentApp(7, "Hong", 3000));
+		student.add(new StudentApp(8, "Ryu", 3500));
+		student.add(new StudentApp(9, "Kang", 5000));
+		student.add(new StudentApp(10, "Jeong", 2500));
+		
+		System.out.println("Student List (ordered by name)");
+		Collections.sort(student);
+		student.forEach(System.out::println);
+		
+		System.out.println("Student List (reverse ordered by name)");
+		Collections.sort(student, Collections.reverseOrder());
+		student.forEach(System.out::println);	
+	}
 
 }
