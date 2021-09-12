fdedaedeadbdccad782f6741d68dc8c904676386
marunemo <chys3697@gmail.com>
2021-09-12 19:13:38 +0900

    Fixed ridiculous part

--- src/prpj_week2/student/StudentApp.java
+++ src/prpj_week2/student/StudentApp.java
@@ -27,16 +27,16 @@ public class StudentApp implements Comparable<StudentApp> {
 	
 	public static void main(String[] args) {
 		ArrayList<StudentApp> student = new ArrayList<StudentApp>();
-		student.add(new StudentApp(1, "Kim", 3500));
-		student.add(new StudentApp(2, "Lee", 2000));
-		student.add(new StudentApp(3, "Park", 5000));
-		student.add(new StudentApp(4, "Choe", 3000));
-		student.add(new StudentApp(5, "Seong", 4500));
-		student.add(new StudentApp(6, "Han", 3500));
-		student.add(new StudentApp(7, "Hong", 3000));
-		student.add(new StudentApp(8, "Ryu", 3500));
-		student.add(new StudentApp(9, "Kang", 5000));
-		student.add(new StudentApp(10, "Jeong", 2500));
+		student.add(new StudentApp(1, "Kim", 35));
+		student.add(new StudentApp(2, "Lee", 20));
+		student.add(new StudentApp(3, "Park", 50));
+		student.add(new StudentApp(4, "Choe", 30));
+		student.add(new StudentApp(5, "Seong", 45));
+		student.add(new StudentApp(6, "Han", 35));
+		student.add(new StudentApp(7, "Hong", 30));
+		student.add(new StudentApp(8, "Ryu", 35));
+		student.add(new StudentApp(9, "Kang", 50));
+		student.add(new StudentApp(10, "Jeong", 25));
 		
 		System.out.println("Student List (ordered by name)");
 		Collections.sort(student);
